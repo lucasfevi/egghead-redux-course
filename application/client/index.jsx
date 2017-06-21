5a467b61d2894504bd5a97ea321fa4daaed66385
Lucas Vieira <lucas.vieira@possible.com>
2017-06-21 14:10:57 -0300

    Add video 12 code

--- application/client/index.jsx
+++ application/client/index.jsx
@@ -11,7 +11,17 @@ const todos = (state = [], action) => {
           text: action.text,
           completed: false,
         }
-      ]
+      ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo;
+        }
+
+        return Object.assign({}, todo, {
+          completed: !todo.completed,
+        });
+      });
     default:
       return state;
   }
@@ -40,6 +50,45 @@ const testAddTodo = () => {
   ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false,
+    },
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1,
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true,
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+}
+
 testAddTodo();
+testToggleTodo();
 
 console.log('All tests passed.');
