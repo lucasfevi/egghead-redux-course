6ab504f3f622fdc40ada96a903100b404860e8de
Lucas Vieira <lucas.vieira@possible.com>
2017-06-21 10:54:57 -0300

    Base application with Docker + Webpack 2 running

---
+++ README.md
@@ -0,0 +1,5 @@
+# egghead.io Redux Course
+
+[Course Link](https://egghead.io/courses/getting-started-with-redux)
+
+**Each video will have its own branch.** But some of them won't, mainly the first ones, since they are simple explanations and do not need coding.
---
+++ application/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
+  devServer: {
+    compress: true,
+    host: '0.0.0.0',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
