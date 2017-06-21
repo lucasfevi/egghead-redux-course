f2ec9be66ea9e2510dadac3aafedc5d0e0d7d71e
Lucas Vieira <lucas.vieira@possible.com>
2017-06-21 11:52:45 -0300

    Add video 7 code

--- application/client/index.jsx
+++ application/client/index.jsx
@@ -1,5 +1,3 @@
-import { createStore } from 'redux';
-
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -11,11 +9,32 @@ const counter = (state = 0, action) => {
   }
 }
 
-const store = createStore(counter);
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
 
-// console.log(store.getState());
-// store.dispatch({ type: 'INCREMENT' });
-// console.log(store.getState());
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+}
+
+const store = createStore(counter);
 
 const render = () => {
   document.body.innerText = store.getState();
