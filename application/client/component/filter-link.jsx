f205e91fb838ef8d96a2057b90774c6b973b3bb6
Lucas Vieira <lucas.vieira@possible.com>
2017-06-22 14:53:08 -0300

    Add video 29 code

---
+++ application/client/component/filter-link.jsx
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import Link from './link';
+
+const mapStateToProps = (state, ownProps) => ({
+  active: ownProps.filter === state.visibilityFilter,
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick: () => {
+    dispatch({
+      type: 'SET_VISIBILITY_FILTER',
+      filter: ownProps.filter,
+    });
+  },
+});
+
+const FilterLink = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Link);
+
+export default FilterLink;
--- application/client/index.jsx
+++ application/client/index.jsx
@@ -1,53 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import TodosReducer from './reducer/todos';
 import VisibilityFilterReducer from './reducer/visibility-filter';
 import AddTodo from './component/add-todo';
-import Link from './component/link';
+import FilterLink from './component/filter-link';
 import VisibleTodoList from './component/visible-todo-list';
 
 const todoApp = combineReducers({
   todos: TodosReducer,
   visibilityFilter: VisibilityFilterReducer,
 });
-
-class FilterLink extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate(),
-    );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={props.filter === state.visibilityFilter}
-        onClick={() => {
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter,
-          });
-        }}
-      >
-        {props.children}
-      </Link>
-    );
-  }
-}
-FilterLink.contextTypes = {
-  store: React.PropTypes.object,
-};
 
 const Footer = () => (
   <p>
